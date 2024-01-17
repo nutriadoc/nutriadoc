1739523bd13e7caf0beb4ab391b68c3b9eb63169
CJ <weicongju@gmail.com>
2024-01-17 13:58:05 +0800

    upgrade version

--- website/src/app/Nutria.tsx
+++ website/src/app/Nutria.tsx
@@ -49,7 +49,7 @@ export interface NutriaProps {
 
 export default async function Nutria(props: NutriaProps) {
   const debug: boolean = false
-  let nutriaUrl = "https://cdn.jsdelivr.net/npm/nutria@0.0.12/dist"
+  let nutriaUrl = "https://cdn.jsdelivr.net/npm/nutria@0.0.13/dist"
   if (debug) {
     nutriaUrl = "http://localhost:4173"
   }
